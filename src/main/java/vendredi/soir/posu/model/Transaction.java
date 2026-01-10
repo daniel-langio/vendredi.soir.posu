837f68dd96a75c63977361c1035577859d8e761f
Mathieu <hei.tafita.2@gmail.com>
2026-01-11 01:16:19 +0300

    feat: add TransactionCategoryRepository

---
+++ src/main/java/vendredi/soir/posu/model/Transaction.java
@@ -0,0 +1,43 @@
+package vendredi.soir.posu.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private Instant date;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+
+    private String walletReference;
+
+    private Double amount;
+
+    private String categoryReference;
+
+    private String subCategoryReference;
+
+    private String description;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+}
---
+++ src/main/java/vendredi/soir/posu/model/Wallet.java
@@ -0,0 +1,32 @@
+package vendredi.soir.posu.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Wallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String reference;
+
+    @Enumerated(EnumType.STRING)
+    private WalletType type;
+
+    @CreationTimestamp
+    private Instant createdAt;
+}
