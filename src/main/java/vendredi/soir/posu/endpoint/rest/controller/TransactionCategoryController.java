f0603f37cf331f0c06cf05d74bac5a53117d12e5
Mathieu <hei.tafita.2@gmail.com>
2026-01-11 02:29:50 +0300

    feat(controller): add TransactionCategoryController with create and getAll endpoints

---
+++ src/main/java/vendredi/soir/posu/endpoint/rest/controller/TransactionCategoryController.java
@@ -0,0 +1,32 @@
+package vendredi.soir.posu.endpoint.rest.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import vendredi.soir.posu.model.TransactionCategory;
+import vendredi.soir.posu.service.TransactionCategoryService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transaction/category")
+@AllArgsConstructor
+public class TransactionCategoryController {
+    private final TransactionCategoryService transactionCategoryService;
+
+    @PostMapping
+    public ResponseEntity<List<TransactionCategory>> createTransactionCategory(@RequestBody List<TransactionCategory> categories) {
+        try {
+            List<TransactionCategory> createdCategories = transactionCategoryService.createCategories(categories);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdCategories);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TransactionCategory>> getAllTransactionCategory() {
+        return ResponseEntity.ok(transactionCategoryService.getAllCategories());
+    }
+}
